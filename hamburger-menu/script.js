08a8af09c99eb757bd6311d46eabd7cf321727ac
paqu <pawel.kusz1993@gmail.com>
2020-09-22 13:51:26 +0200

    Create hamburger-like menu

---
+++ hamburger-menu/script.js
@@ -0,0 +1,5 @@
+function onClickMenu() {
+  document.getElementById("menu-icon").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
